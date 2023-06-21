16f5eca6248a2baac5160d04eab9c64e9480ac49
Arpentage <mlalart@novyspec.com>
2023-06-21 16:16:24 +0200

    RESPONSIVE V1

--- animation.js
+++ animation.js
@@ -2,6 +2,8 @@ function randomIntFromInterval(min, max) { // min and max included
     return Math.floor(Math.random() * (max - min + 1) + min)
 }
 
+//IMAGES ALEATOIRES//
+
 const images = ["img/img_lavandes.svg", "img/img_flowers.png", "img/img_nuages.svg", "img/img_mer.svg"]
 
 const index = randomIntFromInterval(0, 3);
@@ -10,3 +12,15 @@ var image = document.getElementById("img_aleatoire");
 image.src = images[index];
 
 console.log(images[index]);
+
+//BURGER MENU//
+
+icons.AddEventListener("click", () => {
+    nav.ClassList.toggle("active")
+});
+
+links.forEach((link) => {
+    link.addEventListener("click", () => {
+        nav.classList.remove("active");
+    });
+});
